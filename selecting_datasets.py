6f5863597831c8c8e4d74f0d33ca3b974a01a9ae
yanneta <yannet@gmail.com>
2018-11-19 20:40:38 -0800

    selecting datasets

---
+++ selecting_datasets.py
@@ -0,0 +1,74 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge, RidgeCV, Lasso, LassoCV, ElasticNetCV
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+import seaborn as sb
+from numpy.linalg import inv
+
+import pandas as pd
+import numpy as np
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+import random
+
+from pmlb import fetch_data, regression_dataset_names
+
+list_dataset = []
+
+for dataset in regression_dataset_names:
+    X, y = fetch_data(dataset, return_X_y=True, local_cache_dir='/data2/yinterian/pmlb/')
+    if X.shape[0] >= 1000:
+        list_dataset.append(dataset)
+
+
+alphas=[1e-4, 1e-3, 1e-2, 1e-1, 1, 2, 4, 8, 16, 32, 64, 132]
+def other_scores(train_X, test_X, train_y, test_y):
+    
+    N = train_X.shape[1]
+    max_features = np.unique([int(x*N + 1) for x in np.linspace(0.01, 0.99, num = 5)])
+    grid = {'max_features': max_features}
+    rf = RandomForestRegressor(n_estimators=1000, n_jobs = 10)
+    rf_cv = GridSearchCV(estimator = rf, param_grid = grid, cv = 5, verbose=2,
+                         n_jobs = 2)
+    ridge  = RidgeCV(cv=5, alphas=alphas)
+    lasso = ElasticNetCV(cv=5, random_state=0, l1_ratio=1)
+    dt = DecisionTreeRegressor(min_samples_leaf=10)
+    
+    scaler = StandardScaler()
+    train_X = scaler.fit_transform(train_X)
+    test_X = scaler.transform(test_X)
+    
+    rf_cv.fit(train_X, train_y)
+    lasso.fit(train_X, train_y)
+    ridge.fit(train_X, train_y)
+    dt.fit(train_X, train_y)
+    scores = [x.score(test_X, test_y) for x in [rf_cv, ridge, lasso, dt]]
+    return scores
+
+model_str = ["RF", "Ridge", "Lasso", "Cart"]
+
+f = open('comparison.log', 'w+')
+for state in range(1,10):
+    for dataset in list_dataset:
+        X, y = fetch_data(dataset, return_X_y=True, local_cache_dir='/data2/yinterian/pmlb/')
+        train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=state)
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        test_X = scaler.transform(test_X)
+        scores = other_scores(train_X, test_X, train_y, test_y)
+        score_str = ["%s %.4f" % (s, score) for s,score in zip(model_str, scores)]
+        results = "dataset %s state %d %s"  %(dataset, state, score_str)
+        f.write(results)
+        f.write("\n")
+        f.flush()
+f.close()
